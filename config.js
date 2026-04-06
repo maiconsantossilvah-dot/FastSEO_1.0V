2c967f63907c928b69bf8a29bc785fa5fc259089
MaiconSantos <maiconsantossilvah@gmail.com>
2026-04-06 11:48:05 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 export const FIREBASE_CONFIG = {
-  apiKey:            "YOUR_FIREBASE_API_KEY",
-  authDomain:        "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId:         "YOUR_PROJECT_ID",
-  storageBucket:     "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId:             "YOUR_APP_ID",
+  apiKey:            "AIzaSyB7N4AXfSIia5dm_M8_bIx8w01MHy5JgFk",
+  authDomain:        "fastseo-6a61b.firebaseapp.com",
+  projectId:         "fastseo-6a61b",
+  storageBucket:     "fastseo-6a61b.firebasestorage.app",
+  messagingSenderId: "460968097608",
+  appId:             "1:460968097608:web:48b103fc710bd98bff657d",
+  measurementId: "G-5QMCX3H979"
 };
 export const GEMINI_DEFAULT_MODEL = "gemini-2.0-flash";
 export const MISTRAL_MODEL = "mistral-small-latest";
